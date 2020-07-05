5cd63eb53ee68edf3227ef566909214efc56ce7c
Rajat Kotian <rajat@ebullientech.com>
2020-07-05 14:20:57 +0530

    Refactoring Code

--- expense_planner/lib/widgets/chart.dart
+++ expense_planner/lib/widgets/chart.dart
@@ -6,38 +6,35 @@ import 'package:intl/intl.dart';
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
 
-  Chart(this.recentTransactions); 
+  Chart(this.recentTransactions);
 
-  List <Map<String,Object>> get groupedTransactionValues{
-    return List.generate(7, (index){
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
       final weekDay = DateTime.now().subtract(Duration(days: index));
 
       var totalSum = 0.0;
 
-      for (var i = 0 ;i < recentTransactions.length; i++) {
-        if (recentTransactions[i].date.day == weekDay.day && 
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
             recentTransactions[i].date.month == weekDay.month &&
-            recentTransactions[i].date.year == weekDay.year){
-              totalSum += recentTransactions[i].amount;
-
-         }
-
+            recentTransactions[i].date.year == weekDay.year) {
+          totalSum += recentTransactions[i].amount;
+        }
       }
 
       // print(DateFormat.E().format(weekDay));
       // print(totalSum);
 
-      return{
-        'day':DateFormat.E().format(weekDay).substring(0,2),
-        'amount':totalSum,
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 2),
+        'amount': totalSum,
       };
     }).reversed.toList();
   }
 
   double get total_spending {
-    return groupedTransactionValues.fold(0.0, ( sum , item ) {
-      return sum+item['amount'];
-
+    return groupedTransactionValues.fold(0.0, (sum, item) {
+      return sum + item['amount'];
     });
   }
 
@@ -51,19 +48,19 @@ class Chart extends StatelessWidget {
         padding: EdgeInsets.all(10),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: groupedTransactionValues.map((data){
+          children: groupedTransactionValues.map((data) {
             return Flexible(
               fit: FlexFit.tight,
-                        child: ChartBar(data['day'], data['amount'],
-              total_spending == 0.0 ? 0.0:( data['amount'] as double)/total_spending),
+              child: ChartBar(
+                  data['day'],
+                  data['amount'],
+                  total_spending == 0.0
+                      ? 0.0
+                      : (data['amount'] as double) / total_spending),
             );
-          }).toList()
-
-            
-          ,
+          }).toList(),
         ),
       ),
-      
     );
   }
 }
